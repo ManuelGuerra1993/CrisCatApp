c62c2a151fcc36bce8951270745297b38a34d447
ManuelGuerra1993 <manuel19_93@hotmail.com>
2021-09-13 19:42:03 -0500

    Fix

--- app/src/main/java/com/manu/criscatapp/Home.java
+++ app/src/main/java/com/manu/criscatapp/Home.java
@@ -29,7 +29,7 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent  intent = new Intent(Home.this,MainActivity.class);
+                Intent  intent = new Intent(Home.this,ListaMascotaActivity.class);
                 startActivity(intent);
             }
         });
