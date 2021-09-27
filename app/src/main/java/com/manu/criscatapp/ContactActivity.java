4b30fd3ea42bdb90402afee5c0fe6c7399b444f7
ManuelGuerra1993 <manuel19_93@hotmail.com>
2021-09-26 22:45:48 -0500

    Contac

--- app/src/main/java/com/manu/criscatapp/ContactActivity.java
+++ app/src/main/java/com/manu/criscatapp/ContactActivity.java
@@ -2,13 +2,89 @@ package com.manu.criscatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.manu.criscatapp.modelo.Cita;
+import com.manu.criscatapp.modelo.Contacto;
+
+import java.util.UUID;
 
 public class ContactActivity extends AppCompatActivity {
+
+    EditText txtTitulo, txtDetalle;
+    Button btnGuardar;
+
+    FirebaseDatabase fbDataBase;
+    DatabaseReference dbReference;
+
+    String titulo, detalle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        inicializarFirebase();
+        asignarReferencias();
+
+
+    }
+
+    private void asignarReferencias(){
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtDetalle = findViewById(R.id.txtDetalle);
+        btnGuardar = findViewById(R.id.btnSendContact);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Registro();
+            }
+        });
+
+    }
+
+    private void Registro(){
+        titulo = txtTitulo.getText().toString();
+        detalle = txtDetalle.getText().toString();
+
+        if (validarCampos()==true){
+            Contacto c = new Contacto();
+            c.setId(UUID.randomUUID().toString());
+            c.setTitulo(titulo);
+            c.setDetalle(detalle);
+            dbReference.child("Contacto").child(c.getId()).setValue(c);
+            Toast.makeText(this, "Registro Completado", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(ContactActivity.this, Home.class);
+            startActivity(intent);
+        } else {}
+
+    }
+
+    private boolean validarCampos(){
+        boolean retorno = true;
+        if (titulo.isEmpty()){
+            txtTitulo.setError("El título es obligatorio");
+            retorno = false;
+        }
+        if (detalle.isEmpty()){
+            txtDetalle.setError("El Detalle es obligatorio");
+            retorno = false;
+        }
+        return retorno;
+    }
+
+    private void inicializarFirebase(){
+        FirebaseApp.initializeApp(this);
+        fbDataBase = FirebaseDatabase.getInstance();
+        dbReference = fbDataBase.getReference();
     }
 }
---
+++ app/src/main/java/com/manu/criscatapp/modelo/Contacto.java
@@ -0,0 +1,31 @@
+package com.manu.criscatapp.modelo;
+
+public class Contacto {
+    private String id;
+    private String titulo;
+    private String detalle;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+}
